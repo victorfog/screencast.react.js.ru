ad13ff9c7e8747a28db5bdab5b287e7633caa528
Victor Danilenko <victor.fog@gmail.com>
2020-11-20 15:23:45 +0300

    Added Styles. before refactoring to write your own style to remove points

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import posts from '../fixtures'
 import PostList from "./PostList";
+import 'bootstrap/dist/css/bootstrap.css' //подключение стилей
 
 function App() {
-   return(
-    <div>
-        <h1>
-            Posts
-        </h1>
-        <PostList posts = {posts}/>
-    </div>
-   )
+    return (
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-3">
+                    Posts
+                </h1>
+            </div>
+            <PostList posts={posts}/>
+        </div>
+    )
 
 }
 
 
- export default App
+export default App
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,24 +1,29 @@
 import React, {Component} from 'react'
 
-class Article extends Component{
+class Article extends Component {
     state = { //Экспериментальный систаксис. По старинке тут идет описание Constructor
         isOpen: false //редактировать state без setState не рекомендуется
     }
 
     render() {
         const {post} = this.props;
-        const mainText = this.state.isOpen && <section>{post.text}</section> //обязательно оформить в элемент <>
-        return(
-            <div>
-                <h2>
-                    {post.title}
-                    <button onClick={this.handleClick}>
-                        {this.state.isOpen ? 'close' : 'open'}
-                        {this.state.isOpen}
-                    </button>
-                </h2>
-                <h3>{mainText}</h3>
-                <h4>Create Date: {(new Date(post.date)).toDateString()}</h4>
+        const style = {width: '50%'}; // не заработало если поместить прям в литеральной натации
+        const mainText = this.state.isOpen && <section className="card-text">{post.text}</section> //обязательно оформить в элемент <>
+        return (
+            <div className="card mx-auto" style = {style}>
+                <div className="card-header">
+                    <h2>
+                        {post.title}
+                        <button onClick={this.handleClick} className="btn btn-primary btn-lg float-right">
+                            {this.state.isOpen ? 'close' : 'open'}
+                            {this.state.isOpen}
+                        </button>
+                    </h2>
+                </div>
+                <div className="card-body">
+                    <h6 className="card-subtitle text-muted">Create Date: {(new Date(post.date)).toDateString()}</h6>
+                    {mainText}
+                </div>
             </div>
         )
     }
